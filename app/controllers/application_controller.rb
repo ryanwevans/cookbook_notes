2c980f802c40ab9d432c5bf65c5d8a39c9eae92f
ryanwevans <ryanwevans@icloud.com>
2018-02-06 00:05:05 -0800

    initial code in application controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:id]
+    end
+
+    def current_user
+      User.find_by(id: session[:id])
+    end
+  end
+
+end
