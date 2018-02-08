dc950dbff99c62c8c1fe5f558a80aae5c4129b1a
ryanwevans <ryanwevans@icloud.com>
2018-02-08 13:26:22 -0800

    add column and relationship to migrations

--- db/migrate/20180208211608_notes.rb
+++ db/migrate/20180208211608_notes.rb
@@ -1,4 +1,9 @@
 class Notes < ActiveRecord::Migration[5.1]
   def change
+    create_table :notes do |t|
+      t.text :content
+      t.integer :cookbook_id
+      t.integer :user_id
+    end
   end
 end
--- db/migrate/20180208211621_cookbooks.rb
+++ db/migrate/20180208211621_cookbooks.rb
@@ -1,4 +1,9 @@
 class Cookbooks < ActiveRecord::Migration[5.1]
   def change
+    create_table :cookbooks do |t|
+      t.string :cookbook_name
+      t.integer :user_id
+      t.integer :note_id
+    end
   end
 end
