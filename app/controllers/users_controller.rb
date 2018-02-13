b1039f84a427ec75bde663f899d77a094bba0b50
ryanwevans <ryanwevans@icloud.com>
2018-02-12 19:26:12 -0800

    add framework of user controller actions; add signup controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,29 @@
 class UsersController < ApplicationController
-  
+
+  get '/signup' do
+    redirect_unless_logged_in
+    erb ':/users/create_user'
+  end
+
+  post '/signup' do
+    redirect_unless_logged_in
+    redirect '/cookbooks/cookbooks'
+  end
+
+  get '/login' do
+    redirect_unless_logged_in
+
+  end
+
+  post '/login' do
+    redirect_unless_logged_in
+
+  end
+
+  get '/logout' do
+    redirect_unless_logged_in
+    session.clear
+    redirect '/login'
+  end
+
 end
