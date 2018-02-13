3d119329dae110f88aa4e5e7962405ec16061d68
ryanwevans <ryanwevans@icloud.com>
2018-02-12 18:21:29 -0800

    create controller actions

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,3 +1,62 @@
 class NotesController < ApplicationController
 
+    # view all notes
+    get '/notes' do
+      redirect_unless_logged_in
+      erb ':/notes/notes'
+    end
+
+    # view a specific note
+    get '/notes/:id' do
+      redirect_unless_logged_in
+      @note = Note.find(params[:id])
+      erb ':/notes/show_note'
+    end
+
+    # view form to create new note
+    get '/notes/new' do
+      redirect_unless_logged_in
+      erb ':/notes/create_note'
+    end
+
+    # post new note from form to db of all notes
+    post '/notes' do
+      redirect_unless_logged_in
+      @note = Note.create(:content => params[:content])
+      if !params[:cookbook_id].empty?
+        @note.cookbook_id = params[:cookbook_id]
+      end
+      @note.user_id = current_user.id
+      @note.save
+      redirect '/notes'
+    end
+
+    # view form to edit a specific note
+    get '/notes/:id/edit' do
+      redirect_unless_logged_in
+      erb ':/notes/edit_note'
+    end
+
+    # patch updated note info from edit form to specific note
+    patch '/notes/:id' do
+      redirect_unless_logged_in
+      @note = Note.find(params[:id])
+      @note.content = params[:content]
+      if !params[:cookbook_id].empty?
+        @note.cookbook_id = params[:cookbook_id]
+      end
+      @note.save
+      redirect '/notes'
+    end
+
+    # delete a specific note from all notes
+    delete '/notes/:id' do
+      redirect_unless_logged_in
+      @note = Note.find(params[:id])
+      if current_user.id == @note.user_id
+        @note.delete
+      end
+      redirect '/notes'
+    end
+
 end
