60d5ca6e14d0a0e65f69a85c32dda8af097ad40d
ryanwevans <ryanwevans@icloud.com>
2018-02-12 17:28:22 -0800

    add redirect unless logged in; build out create and edit controllers

--- app/controllers/cookbooks_controller.rb
+++ app/controllers/cookbooks_controller.rb
@@ -2,32 +2,46 @@ class CookbooksController < ApplicationController
 
   # view all cookbooks
   get '/cookbooks' do
+    redirect_unless_logged_in
     erb ':/cookbooks/cookbooks'
   end
 
   # view a specific cookbook
   get '/cookbooks/:id' do
+    redirect_unless_logged_in
+    @cookbook = Cookbook.find(params[:id])
     erb ':/cookbooks/show_cookbook'
   end
 
   # view form to create new cookbook
   get '/cookbooks/new' do
+    redirect_unless_logged_in
     erb ':/cookbooks/create_cookbook'
   end
 
   # post new cookbook from form to db of all cookbooks
   post '/cookbooks' do
-
+    redirect_unless_logged_in
+    @cookbook = Cookbook.create(:cookbook_name => params[:cookbook_name])
+    @cookbook.user_id = current_user.id
+    @cookbook.save
+    redirect '/cookbooks'
   end
 
   # view form to edit a specific cookbook
   get '/cookbooks/:id/edit' do
+    redirect_unless_logged_in
     erb ':/cookbooks/edit_cookbook'
   end
 
   # patch updated cookbook info from edit form to specific cookbook
   patch '/cookbooks/:id' do
-
+    redirect_unless_logged_in
+    @cookbook = Cookbook.find(params[:id])
+    @cookbook.cookbook_name = params[:cookbook_name]
+    @cookbook.note_id = params[:note_id]
+    @cookbook.save
+    redirect '/cookbooks'
   end
 
 end
