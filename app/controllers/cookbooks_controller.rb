5c2b327cf8eade751ecb1c609a2f454cca965296
ryanwevans <ryanwevans@icloud.com>
2018-02-12 16:49:17 -0800

    add framework for cookbook controller actions

--- app/controllers/cookbooks_controller.rb
+++ app/controllers/cookbooks_controller.rb
@@ -1,11 +1,33 @@
 class CookbooksController < ApplicationController
 
+  # view all cookbooks
   get '/cookbooks' do
     erb ':/cookbooks/cookbooks'
   end
 
+  # view a specific cookbook
+  get '/cookbooks/:id' do
+    erb ':/cookbooks/show_cookbook'
+  end
+
+  # view form to create new cookbook
   get '/cookbooks/new' do
     erb ':/cookbooks/create_cookbook'
+  end
+
+  # post new cookbook from form to db of all cookbooks
+  post '/cookbooks' do
+
+  end
+
+  # view form to edit a specific cookbook
+  get '/cookbooks/:id/edit' do
+    erb ':/cookbooks/edit_cookbook'
+  end
+
+  # patch updated cookbook info from edit form to specific cookbook
+  patch '/cookbooks/:id' do
+
   end
 
 end
