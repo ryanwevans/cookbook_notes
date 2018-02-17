8da99d9f9667f5e6416351714c8d68cf74cba9ca
ryanwevans <ryanwevans@icloud.com>
2018-02-16 22:39:19 -0800

    add signup, login; remove validate logged in for those pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,10 @@
 class UsersController < ApplicationController
 
   get '/signup' do
-    redirect_unless_logged_in
-    erb ':/users/create_user'
+    erb :'/users/create_user'
   end
 
   post '/signup' do
-    redirect_unless_logged_in
     @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
     if @user && @user.save
       session[:id] = @user.id
@@ -18,12 +16,10 @@ class UsersController < ApplicationController
   end
 
   get '/login' do
-    redirect_unless_logged_in
     erb :'users/login'
   end
 
   post '/login' do
-    redirect_unless_logged_in
     @user = User.find_by(:username => params[:username])
     if @user && @user.authenticate(params[:password])
       session[:id] = @user.id
