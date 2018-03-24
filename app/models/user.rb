65ad19d633b1ea127b1f16130a000df4bf1e14db
ryanwevans <ryanwevans@icloud.com>
2018-03-23 17:19:37 -0700

    clean up code; remove unneeded slug methods, delete show note file

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,4 @@ class User < ActiveRecord::Base
   has_many :notes
   has_secure_password
   validates_presence_of :username, :email, :password
-
-  def slug
-  	self.username.slugify
-  end
-
-  def self.find_by_slug(slug)
-    User.all.detect{|user| user.slug == slug}
-  end
-
 end
